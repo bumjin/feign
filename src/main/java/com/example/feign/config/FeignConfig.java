bf22441c9f076760d2ab16474be586c764c6fca7
Beomjin Park <bumjin@gmail.com>
2024-03-13 00:00:57 +0900

    로거 추가
    Feign의 로거 추가
    Slow API 로그 추가

--- src/main/java/com/example/feign/config/FeignConfig.java
+++ src/main/java/com/example/feign/config/FeignConfig.java
@@ -1,13 +1,21 @@
 package com.example.feign.config;
 
 import com.example.feign.feign.interceptor.DemoFeignInterceptor;
+import com.example.feign.feign.logger.FeignCustomLogger;
+import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
     @Bean
-    public DemoFeignInterceptor feignInterceptor() {
-        return DemoFeignInterceptor.of();
+    public Logger feignLogger() {
+        return new FeignCustomLogger();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
